dd510c6f93faeafd6e637b98e3623d8d8326f762
arijitgh <arijitghosh73@gmail.com>
2022-02-26 21:03:25 -0500

    Initial commit

---
+++ src/com/arijit/java/example/collections/Main.java
@@ -0,0 +1,9 @@
+package com.arijit.java.example.collections;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello All");
+    }
+}
